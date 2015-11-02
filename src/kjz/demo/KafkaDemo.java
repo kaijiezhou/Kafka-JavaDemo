7204c2fe16799a9aab9a235bf34526c5654d3200
VictorDu <duwentaoabc@gmail.com>
2015-11-01 20:19:23 -0800

    add kafka spark

--- src/kjz/demo/KafkaDemo.java
+++ src/kjz/demo/KafkaDemo.java
@@ -15,68 +15,7 @@ import kjz.demo.spark.SparkStreamDemo;
 
 public class KafkaDemo {
 	public static void main(String[] args) throws IOException {
-		switch(args[0]){
-		case "consumer":
-			String zooKeeper=args[1];
-			String groupId="group0";
-			String topic="test";
-			switch(args[2]){
-			case "single":
-				ConsumerDemo example = new ConsumerDemo(zooKeeper, groupId, topic+"1");
-				example.runSingleConsumer();
-				break;
-			case "multi":
-				int num = Integer.parseInt(args[3]);
-				ConsumerDemo demoThread = new ConsumerDemo(zooKeeper, groupId, topic);
-				demoThread.run(num,3);
-				//demoThread.shutdown(10000000);
-				break;
-			default:
-				System.out.println("Choose your mode:[single], or [multi] [num]");
-			}
-	        break;
-		case "producer":
-			String brokerList =args[1];
-			String mode=args[2];
-			ProducerDemo demo=new ProducerDemo(brokerList);
-			switch(mode){
-			case "listener":
-				ServerSocket listener = null;
-				try {
-					listener = new ServerSocket(9090);
-					while (true) {
-		                Socket socket = listener.accept();
-		                try {
-		                	PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-		                	BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		                	String line;
-		                    while((line=in.readLine())!= null){
-		                    	System.out.println(line);
-		                    	demo.runListenerDemo("test", "key",line);
-		                    }
-		                    in.close();
-		                } finally {
-		                    socket.close();
-		                }
-		                break;
-		            }
-				}finally{
-		            listener.close();
-					demo.Close_Producer();
-					
-				}
-			case "generater":
-				demo.runGeneraterDemo(Integer.parseInt(args[3]));
-				break;
-			default:
-				System.out.println("Choose your mode: [listener], or [generator] [nums]");
-			}
-			
-			break;
-		default:
-			System.out.println("Choose your role, [consumer] or [producer]?");
-		}
-       
-    }
-
+		SparkStreamDemo demo = new SparkStreamDemo("192.168.0.105:9092,192.168.0.106:9092,192.168.0.107:9092,192.168.0.108:9092", "topic1,topic2");
+		demo.runTerminalDemo();
+	}
 }
